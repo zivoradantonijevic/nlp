8400752930b21a338ba2b036289bfc007420e5c8
Zivorad <zivoradantonijevic@gmail.com>
2015-09-22 01:05:16 +0200

    very initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import simplenlg.framework.NLGElement;
+import simplenlg.framework.NLGFactory;
+import simplenlg.lexicon.Lexicon;
+import simplenlg.realiser.english.Realiser;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lexicon lexicon = Lexicon.getDefaultLexicon();
+        NLGFactory nlgFactory = new NLGFactory(lexicon);
+        Realiser realiser = new Realiser(lexicon);
+
+        NLGElement s1 = nlgFactory.createSentence("my dog is happy");
+        String output = realiser.realiseSentence(s1);
+        System.out.println(output);
+    }
+}
